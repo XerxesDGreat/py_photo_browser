0231c22034d27f88eb05ecd470b162019fdce1c2
Josh Wickham <josh@i-josh.com>
2013-09-10 08:45:51 -0700

    update database functions to be more resilient; add FetchFieldArg class for better query representation; update model to reflect some changes to DB and allow for better importing/creation

--- wsgi/src/database.py
+++ wsgi/src/database.py
@@ -3,6 +3,7 @@ import MySQLdb
 from logger import Logger
 
 class Database:
+	DB_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 	_db = None
 	_cursor = None
 
@@ -33,18 +34,28 @@ class Database:
 		return Database._do_query(query)
 
 	@staticmethod
-	def fetch_by_properties(table_name, property_dict, limit=None, offset=None):
+	def fetch_by_properties(table_name, fields, limit=None, offset=None):
 		Database._init()
-		field_value_list = []
-		for field_name, value in property_dict.iteritems():
-			field_value_list.append("`%s` = \"%s\"" % (field_name, value))
 		limit_stmt = "" if limit == None else "LIMIT=%d" % limit
 		offset_stmt = "" if offset == None else "OFFSET=%d" % offset
 		query = "SELECT * FROM `%s` WHERE %s %s %s;" % (
 			table_name,
-			" AND ".join(field_value_list),
+			" AND ".join([f.get_query_string() for f in fields]),
 			limit_stmt,
 			offset_stmt
+		)
+		return Database._do_query(query)
+	
+	@staticmethod
+	def fetch_unique_field_values(table_name, unique_field, criteria_fields = []):
+		Database._init()
+		where_stmt = ""
+		if len(criteria_fields) > 0:
+			where_stmt = "WHERE %s" % " AND ".join([f.get_query_string() for f in criteria_fields])
+		query = "SELECT DISTINCT %s from `%s` %s;" % (
+			unique_field.get_query_string(),
+			table_name,
+			where_stmt
 		)
 		return Database._do_query(query)
 
@@ -80,23 +91,65 @@ class Database:
 		)
 		value_tuple = tuple(value_list)
 		Logger.debug(query)
-		return Database._do_query(query, [value_tuple])
+		resp = Database._do_query(query, value_tuple)
+		return Database._cursor.lastrowid
 
 	@staticmethod
-	def _do_query(query, values = [], commit = True):
-		Logger.debug(query)
+	def _do_query(query, values = None, commit = True):
+		Logger.debug("Executing query: %s" % query)
 		results = []
 		try:
 			Database._cursor.execute(query, values)
 			if commit:
 				Database._db.commit()
 			results = Database._cursor.fetchall()
-			Logger.debug(results)
 
 		except Exception, e:
 			Logger.debug("database exception caught: %s" % str(e))
+			Logger.debug("query: %s, values: %s, commit: %s" % (
+				str(query),str(values), str(commit)))
 			Logger.error('some exception caught')
 			Database._db.rollback()
 
-		Logger.debug(repr(results))
 		return results
+
+class FetchFieldArg():
+	CMP_LT = "<"
+	CMP_GT = ">"
+	CMP_LTE = "<="
+	CMP_GTE = ">="
+	CMP_EQ = "="
+	CMP_NE = "<>"
+	CMP_BETWEEN = "BETWEEN"
+	CMP_IN = "IN"
+
+	DB_OP_YEAR = "YEAR"
+	DB_OP_MONTH = "MONTH"
+	DB_OP_DAY = "DAY"
+
+	def __init__(self, field, compare_operation = None, compare_value = None, db_op = None):
+		self._field = field
+		self._compare_operation = compare_operation
+		self._compare_value = compare_value
+		self._db_op = db_op
+
+	def get_query_string(self):
+		if self._db_op == None:
+			field_str = "`%s`" % self._field
+		else:
+			field_str = "%s(`%s`)" % (self._db_op, self._field)
+		if self._compare_operation == FetchFieldArg.CMP_BETWEEN:
+			str = "%s BETWEEN '%s' AND '%s'" % (
+				field_str,
+				self._compare_value[0],
+				self._compare_value[1]
+			)
+		elif self._compare_operation != None:
+			str = "%s %s '%s'" % (
+				field_str,
+				self._compare_operation,
+				self._compare_value
+			)
+		else:
+			str = field_str
+		return str
--- wsgi/src/model.py
+++ wsgi/src/model.py
@@ -1,11 +1,14 @@
 from logger import Logger
 from settings import Settings as S
 from database import Database as DB
+from database import FetchFieldArg
 
 import os
 import Image
 import util
 import exifread
+import datetime
+import time
 
 class Photo():
 	SMALL_THUMB_SIZE = (200, 200)
@@ -26,27 +29,115 @@ class Photo():
 		exifread.tags.EXIF_TAGS[ORIENTATION_KEY][1][8]: 90
 	}
 
+	GUARD = {}
+
+	############################################################################
+	# FETCHING METHODS
+	############################################################################
 	@staticmethod
 	def get_by_id(id):
-		row = DB.fetch_by_properties(Photo.DB_TABLE_NAME, {"id": 1})
-		Logger.debug(str(row))
-		return row
+		id_field = FetchFieldArg("id", FetchFieldArg.CMP_EQ, id)
+		row = DB.fetch_by_properties(Photo.DB_TABLE_NAME, [id_field])
+		p = Photo(Photo.GUARD, row[0])
+		return p
 	
+	@staticmethod
+	def get_by_date(timestamp=None, year=None, month=None, day=None, field="image_date", limit=None, offset=None):
+		"""
+		Fetches a list of photos based on the given date.
+
+		timestamp (int) will supercede the year, month, day values.
+		Valid combinations are:
+		- timestamp
+		- year
+		- year & month
+		- year & month & day
+		If just a day or just a month or just a month & day is given, will return
+		an empty list
+		"""
+		query_args = []
+		if timestamp != None:
+			dt = datetime.date.fromtimestamp(timestamp)
+			query_args.append(FetchFieldArg(field, FetchFieldArg.CMP_EQ, dt.year, FetchFieldArg.DB_OP_YEAR))
+			query_args.append(FetchFieldArg(field, FetchFieldArg.CMP_EQ, dt.month, FetchFieldArg.DB_OP_MONTH))
+			query_args.append(FetchFieldArg(field, FetchFieldArg.CMP_EQ, dt.day, FetchFieldArg.DB_OP_DAY))
+		else:
+			if year == None:
+				pass
+			elif year != None and day != None and month == None:
+				pass
+			else:
+				query_args.append(FetchFieldArg(field, FetchFieldArg.CMP_EQ, year, FetchFieldArg.DB_OP_YEAR))
+				if month != None:
+					query_args.append(FetchFieldArg(field, FetchFieldArg.CMP_EQ, month, FetchFieldArg.DB_OP_MONTH))
+				if day != None:
+					query_args.append(FetchFieldArg(field, FetchFieldArg.CMP_EQ, day, FetchFieldArg.DB_OP_DAY))
+		rows = DB.fetch_by_properties(Photo.DB_TABLE_NAME, query_args)
+		Logger.debug("all the rows: %s" % str(rows))
+		resp = []
+		for i in range(0, len(rows)):
+			Logger.debug("%s" % str(rows[i]))
+			resp.append(Photo(Photo.GUARD, rows[i]))
+		return resp
+
+	@staticmethod
+	def get_all_years():
+		year_field = FetchFieldArg("image_date", None, None, FetchFieldArg.DB_OP_YEAR)
+		results = DB.fetch_unique_field_values(Photo.DB_TABLE_NAME, year_field)
+		return [f[0] for f in results]
+	
+	@staticmethod
+	def get_all_months_by_year(year):
+		year_field = FetchFieldArg("image_date", FetchFieldArg.CMP_EQ, year, FetchFieldArg.DB_OP_YEAR)
+		criteria = [year_field]
+		month_field = FetchFieldArg("image_date", None, None, FetchFieldArg.DB_OP_MONTH)
+		results = DB.fetch_unique_field_values(Photo.DB_TABLE_NAME, month_field, criteria)
+		return [f[0] for f in results]
+
+	@staticmethod
+	def get_all_days_by_ym(year, month):
+		year_field = FetchFieldArg("image_date", FetchFieldArg.CMP_EQ, year, FetchFieldArg.DB_OP_YEAR)
+		month_field = FetchFieldArg("image_date", FetchFieldArg.CMP_EQ, month, FetchFieldArg.DB_OP_MONTH)
+		criteria = [year_field, month_field]
+		day_field = FetchFieldArg("image_date", None, None, FetchFieldArg.DB_OP_DAY)
+		results = DB.fetch_unique_field_values(Photo.DB_TABLE_NAME, day_field, criteria)
+		return [f[0] for f in results]	
+		
+	@staticmethod
 	def get_by_path(path):
 		"""
 		Fetches a list of photos from the given path, paginated as indicated
 		"""
 		full_path = os.path.join(S.BASE_FS_PATH, path)
+	
+	############################################################################
+	# CREATION METHODS
+	############################################################################
+	@staticmethod
+	def create(file_basename, path, hash, created_struct):
+		image_date = time.strftime(DB.DB_DATE_FORMAT, created_struct)
+		value_list = [
+			("filename", file_basename, "%s"),
+			("path", path, "%s"),
+			("hash", hash, "%s"),
+			("image_date", image_date, "%s")
+		]
+		inserted_id = DB.create(Photo.DB_TABLE_NAME, value_list)
+		Logger.debug("insert results: %s" % str(inserted_id))
+		return Photo.get_by_id(inserted_id)
 		
 
-	def __init__(self, path, filename):
-		self.path = path
-		self.filename = filename
-		self.id = ""
-		self.mark = ""
-		self.added = ""
-		self.modified = ""
-		self.hash = ""
+	def __init__(self, guard, db_row): 
+		if guard != Photo.GUARD:
+			raise Exception("Must only call __init__ from the accessor methods in Photo")
+		self.id = db_row[0]
+		self.filename = db_row[1]
+		self.path = db_row[2]
+		self.marked = db_row[3]
+		self.added = db_row[4]
+		self.modified = db_row[5]
+		self.hash = db_row[6]
+		self.image_date = db_row[7]
 		self.dirty = False
 
 	def get_or_create_thumb(self, size = SMALL_THUMB_SIZE, path_only = False):
@@ -105,6 +196,10 @@ class Photo():
 		return os.path.basename(self.path)
 	
 	@property
+	def thumb_url(self):
+		return "adsf.jpg"
+	
+	@property
 	def rel_path(self, base_path = S.BASE_FS_PATH):
 		return os.path.relpath(self.path, S.BASE_FS_PATH)
 
@@ -112,3 +207,5 @@ class Photo():
 	def path(self):
 		return self.path
 	
+	def __str__(self):
+		return "%s %s %s %s %s" % (self.path, self.filename, self.added, self.modified, self.image_date)
